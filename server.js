6254af4c218fcb0b62e7e980bee9e72df6d03780
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2023-05-07 19:44:30 +0530

    GET requests ,middleware

---
+++ server.js
@@ -0,0 +1,64 @@
+const express=require('express')
+const app=express()
+const PORT=3000
+const cars=[{
+    id:1,
+    name:"Ferarri"
+},
+{
+    id:2,
+    name:"Redbull"
+},{
+    id:3,
+    name:"Mercedes"
+
+}]
+//middelware
+app.use((req,res,next)=>{
+    const start=Date.now()
+    next();
+    const delta=Date.now()
+    console.log(`${req.url},${req.method},${delta}`)
+    
+})
+app.get('/cars/:carid',(req,res)=>{
+    const carid=Number(req.params.carid);
+    const car=cars[carid]
+    if(car){
+        res.json(car)
+    }else{
+        res.json("Error not found")
+    }
+    
+})
+app.get('/',(req,res)=>{
+    res.send({
+        id:1,
+        name:'Ferarri'
+    },
+)
+})
+app.get('/m',(req,res)=>{
+    res.send({
+        id:2,
+        name:'Mercedes'
+    },
+)
+})
+app.use((express.json()));//inbuilt middleware
+app.post('/cars',(req,res)=>{
+    if(!req.body.name){
+        res.status(400).json({
+            error:"Bad Request"
+        })
+    }
+    const new_car={
+     name:req.body.name,
+     id:cars.length
+    }
+    cars.push(new_car);
+})
+app.listen(PORT,()=>{
+    console.log(`listening on ${PORT}`)
+})
+//gave two routed parameters now we will try to do it ia an array of json objects
